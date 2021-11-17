a781551ba270d0479cb2c4250090d1382efa8469
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2021-11-17 18:02:26 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -902,6 +902,7 @@ $(window).on('load', function() {
    */
   function changeAttribution() {
     var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
+    var credit = 'View <a href="' + googleDocURL + '" target="_blank">data</a>';
     var name = getSetting('_authorName');
     var url = getSetting('_authorURL');
 
